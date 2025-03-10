58e9d61d4efb2a550ac34184bbe530b4dbf6dd32
Caden Peterson <57927858+c4den@users.noreply.github.com>
2025-03-10 17:25:47 -0400

    Initial Tab Creation
    Created tabs in the methods outlined by Expo

--- LookUp/app/(tabs)/_layout.tsx
+++ LookUp/app/(tabs)/_layout.tsx
@@ -7,6 +7,7 @@ import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { Ionicons } from "@expo/vector-icons"; // Import the icons we intend to use
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -27,17 +28,52 @@ export default function TabLayout() {
         }),
       }}>
       <Tabs.Screen
+        name="search"
+        options={{ 
+          title: 'Search', 
+          tabBarIcon: ({ color }) => 
+          <Ionicons
+              name='search'
+              size={24}
+              color={color}
+          /> }}
+      />
+      <Tabs.Screen
+        name="favorites"
+        options={{ 
+          title: 'Favorites', 
+          tabBarIcon: ({ color }) => 
+          <Ionicons
+              name='star'
+              size={24}
+              color={color}
+          /> }}
+      />
+      <Tabs.Screen
+        name="map"
+        options={{ 
+          title: 'Map', 
+          tabBarIcon: ({ color }) => 
+          <Ionicons
+              name="map"
+              size={24}
+              color={color}
+          /> }}
+      />
+      <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Home (will be account)',
+          tabBarIcon: ({ color }) => 
+          <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Explore (will be settings)',
+          tabBarIcon: ({ color }) => 
+          <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
     </Tabs>
