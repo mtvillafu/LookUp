f9832fecae0757c441f00d1b913857f4635d192d
Alperen Yazmaci <alperen.yazmaci@gmail.com>
2025-04-06 03:25:00 -0400

    YOLOv8 implementation and API endpoint done

--- api/express/app.js
+++ api/express/app.js
@@ -1,13 +1,38 @@
 const express = require('express');
+const multer = require('multer');
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
+const upload = multer({ dest: 'uploads/' });
 
-app.listen(PORT, (error) =>{
-    if(!error)
-        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
-    else 
-        console.log("Error occurred, server can't start", error);
-    }
-);
+app.post('/proxy-detect', upload.single('image'), async (req, res) => {
+  try {
+    confidence = 0.5
+    iou = 0.3
+
+    const form = new FormData();
+    form.append('image', fs.createReadStream(req.params));
+    form.append('confidence', confidence);
+    form.append('iou', iou);
+
+    const response = await axios.post('http://localhost:5001/detect-and-annotate', form, {
+      headers: form.getHeaders(),
+      responseType: 'stream',
+    });
+
+    res.set('Content-Type', 'image/jpeg');
+    response.data.pipe(res);
+  } catch (error) {
+    console.error('Error calling Python API:', error.message);
+    res.status(500).send('Error processing image');
+  } 
+});
+
+app.listen(PORT, () => {
+  console.log(`Express API running at http://localhost:${PORT}`);
+});
